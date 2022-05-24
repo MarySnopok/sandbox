4b886de3f4267cdaf224561c0f7cf18c7bff771b
Mary Snopok <Mary.Snopok@proffsmagasinet.se>
2022-05-24 12:41:08 +0200

    swapping branches

--- src/Client/NavigationF.fs
+++ src/Client/NavigationF.fs
@@ -1,1 +1,25 @@
+module NavigationF
 
+open Elmish
+
+open Elmish.UrlParser
+open Product
+open Mainmodel
+open IndexF
+open View
+
+
+let pageParser : Parser<_, Mainmodel.Page> =
+     oneOf [
+            map Mainmodel.Page.Product (s "product")
+            map Mainmodel.Page.IndexF (s "index")
+        ]
+        
+let urlUpdate (page: Mainmodel.Page option) _ =
+    let page = page |> Option.defaultValue Mainmodel.Page.index
+
+    let model, _ = Some page |> Mainmodel.init
+
+    { model with
+            CurrentPage = page;
+            SubModel = model.SubModel }, Cmd.none
